3ba34347ed75e452aa16f0d03c4d81b136812902
ldc <40616595+lcpdecastro@users.noreply.github.com>
2024-02-22 22:23:26 +0800

    Minor changes

--- src/lib/components/arcade/Score.svelte
+++ src/lib/components/arcade/Score.svelte
@@ -1,4 +1,7 @@
 <script>
+  import flip from '$lib/js/flipTransition.js';
+  import { cssEaseIn, cssEaseOut } from '$lib/js/cssEase.js';
+
   let score = $state(0);
 
   export function reset () {
@@ -11,7 +14,11 @@
 </script>
 
 <div class="wrapper">
-  <span class="score">{ score }</span>
+  <div class="score-wrapper">
+    { #key score }
+      <span class="score" in:flip={ { duration: 300, easing: cssEaseIn } } out:flip={ { duration: 300, easing: cssEaseOut, from: 0, to: 180 } }>{ score }</span>
+    { /key }
+  </div>
   <span>SOLVED</span>
 </div>
 
@@ -23,11 +30,19 @@
     flex-grow: 1;
   }
 
+  .score-wrapper {
+    display: grid;
+    align-items: center;
+    justify-items: center;
+  }
+
   span {
+    grid-area: 1 / 1 / 2 / 2;
     font-size: 0.75rem;
     font-weight: bold;
     color: var(--theme-color);
     line-height: 100%;
+    backface-visibility: hidden;
   }
   
   .score {
